462ed19e26837c150af237c6703a2d7223daa981
Kosta24 <109385124+Kosta24@users.noreply.github.com>
2023-04-12 15:15:33 +0200

    Add files via upload

---
+++ bin/Release/dep/Main.cpp.d
@@ -0,0 +1,239 @@
+bin/Release/obj/Main.cpp.o: src/Main.cpp src/background.h \
+ lib/SFML/Graphics.hpp lib/SFML/Window.hpp lib/SFML/System.hpp \
+ lib/SFML/Config.hpp lib/SFML/System/Clock.hpp lib/SFML/System/Export.hpp \
+ lib/SFML/System/Time.hpp lib/SFML/System/Err.hpp \
+ lib/SFML/System/FileInputStream.hpp lib/SFML/System/InputStream.hpp \
+ lib/SFML/System/NonCopyable.hpp lib/SFML/System/Lock.hpp \
+ lib/SFML/System/MemoryInputStream.hpp lib/SFML/System/Mutex.hpp \
+ lib/SFML/System/Sleep.hpp lib/SFML/System/String.hpp \
+ lib/SFML/System/Utf.hpp lib/SFML/System/Utf.inl \
+ lib/SFML/System/String.inl lib/SFML/System/Thread.hpp \
+ lib/SFML/System/Thread.inl lib/SFML/System/ThreadLocal.hpp \
+ lib/SFML/System/ThreadLocalPtr.hpp lib/SFML/System/ThreadLocalPtr.inl \
+ lib/SFML/System/Vector2.hpp lib/SFML/System/Vector2.inl \
+ lib/SFML/System/Vector3.hpp lib/SFML/System/Vector3.inl \
+ lib/SFML/Window/Clipboard.hpp lib/SFML/Window/Export.hpp \
+ lib/SFML/Window/Context.hpp lib/SFML/Window/GlResource.hpp \
+ lib/SFML/Window/ContextSettings.hpp lib/SFML/Window/Cursor.hpp \
+ lib/SFML/Window/Event.hpp lib/SFML/Window/Joystick.hpp \
+ lib/SFML/Window/Keyboard.hpp lib/SFML/Window/Mouse.hpp \
+ lib/SFML/Window/Sensor.hpp lib/SFML/Window/Touch.hpp \
+ lib/SFML/Window/VideoMode.hpp lib/SFML/Window/Window.hpp \
+ lib/SFML/Window/WindowHandle.hpp lib/SFML/Window/WindowStyle.hpp \
+ lib/SFML/Graphics/BlendMode.hpp lib/SFML/Graphics/Export.hpp \
+ lib/SFML/Graphics/CircleShape.hpp lib/SFML/Graphics/Shape.hpp \
+ lib/SFML/Graphics/Drawable.hpp lib/SFML/Graphics/RenderStates.hpp \
+ lib/SFML/Graphics/Transform.hpp lib/SFML/Graphics/Rect.hpp \
+ lib/SFML/Graphics/Rect.inl lib/SFML/Graphics/Transformable.hpp \
+ lib/SFML/Graphics/VertexArray.hpp lib/SFML/Graphics/Vertex.hpp \
+ lib/SFML/Graphics/Color.hpp lib/SFML/Graphics/PrimitiveType.hpp \
+ lib/SFML/Graphics/ConvexShape.hpp lib/SFML/Graphics/Font.hpp \
+ lib/SFML/Graphics/Glyph.hpp lib/SFML/Graphics/Texture.hpp \
+ lib/SFML/Graphics/Image.hpp lib/SFML/Graphics/RectangleShape.hpp \
+ lib/SFML/Graphics/RenderTarget.hpp lib/SFML/Graphics/View.hpp \
+ lib/SFML/Graphics/RenderTexture.hpp lib/SFML/Graphics/RenderWindow.hpp \
+ lib/SFML/Graphics/Shader.hpp lib/SFML/Graphics/Glsl.hpp \
+ lib/SFML/Graphics/Glsl.inl lib/SFML/Graphics/Sprite.hpp \
+ lib/SFML/Graphics/Text.hpp lib/SFML/Graphics/VertexBuffer.hpp \
+ lib/SFML/Audio.hpp lib/SFML/Audio/InputSoundFile.hpp \
+ lib/SFML/Audio/Export.hpp lib/SFML/Audio/Listener.hpp \
+ lib/SFML/Audio/Music.hpp lib/SFML/Audio/SoundStream.hpp \
+ lib/SFML/Audio/SoundSource.hpp lib/SFML/Audio/AlResource.hpp \
+ lib/SFML/Audio/OutputSoundFile.hpp lib/SFML/Audio/Sound.hpp \
+ lib/SFML/Audio/SoundBuffer.hpp lib/SFML/Audio/SoundBufferRecorder.hpp \
+ lib/SFML/Audio/SoundRecorder.hpp lib/SFML/Audio/SoundFileFactory.hpp \
+ lib/SFML/Audio/SoundFileFactory.inl lib/SFML/Audio/SoundFileReader.hpp \
+ lib/SFML/Audio/SoundFileWriter.hpp src/obstacle.h src/player.h \
+ src/score.h src/settings.h
+
+src/background.h:
+
+lib/SFML/Graphics.hpp:
+
+lib/SFML/Window.hpp:
+
+lib/SFML/System.hpp:
+
+lib/SFML/Config.hpp:
+
+lib/SFML/System/Clock.hpp:
+
+lib/SFML/System/Export.hpp:
+
+lib/SFML/System/Time.hpp:
+
+lib/SFML/System/Err.hpp:
+
+lib/SFML/System/FileInputStream.hpp:
+
+lib/SFML/System/InputStream.hpp:
+
+lib/SFML/System/NonCopyable.hpp:
+
+lib/SFML/System/Lock.hpp:
+
+lib/SFML/System/MemoryInputStream.hpp:
+
+lib/SFML/System/Mutex.hpp:
+
+lib/SFML/System/Sleep.hpp:
+
+lib/SFML/System/String.hpp:
+
+lib/SFML/System/Utf.hpp:
+
+lib/SFML/System/Utf.inl:
+
+lib/SFML/System/String.inl:
+
+lib/SFML/System/Thread.hpp:
+
+lib/SFML/System/Thread.inl:
+
+lib/SFML/System/ThreadLocal.hpp:
+
+lib/SFML/System/ThreadLocalPtr.hpp:
+
+lib/SFML/System/ThreadLocalPtr.inl:
+
+lib/SFML/System/Vector2.hpp:
+
+lib/SFML/System/Vector2.inl:
+
+lib/SFML/System/Vector3.hpp:
+
+lib/SFML/System/Vector3.inl:
+
+lib/SFML/Window/Clipboard.hpp:
+
+lib/SFML/Window/Export.hpp:
+
+lib/SFML/Window/Context.hpp:
+
+lib/SFML/Window/GlResource.hpp:
+
+lib/SFML/Window/ContextSettings.hpp:
+
+lib/SFML/Window/Cursor.hpp:
+
+lib/SFML/Window/Event.hpp:
+
+lib/SFML/Window/Joystick.hpp:
+
+lib/SFML/Window/Keyboard.hpp:
+
+lib/SFML/Window/Mouse.hpp:
+
+lib/SFML/Window/Sensor.hpp:
+
+lib/SFML/Window/Touch.hpp:
+
+lib/SFML/Window/VideoMode.hpp:
+
+lib/SFML/Window/Window.hpp:
+
+lib/SFML/Window/WindowHandle.hpp:
+
+lib/SFML/Window/WindowStyle.hpp:
+
+lib/SFML/Graphics/BlendMode.hpp:
+
+lib/SFML/Graphics/Export.hpp:
+
+lib/SFML/Graphics/CircleShape.hpp:
+
+lib/SFML/Graphics/Shape.hpp:
+
+lib/SFML/Graphics/Drawable.hpp:
+
+lib/SFML/Graphics/RenderStates.hpp:
+
+lib/SFML/Graphics/Transform.hpp:
+
+lib/SFML/Graphics/Rect.hpp:
+
+lib/SFML/Graphics/Rect.inl:
+
+lib/SFML/Graphics/Transformable.hpp:
+
+lib/SFML/Graphics/VertexArray.hpp:
+
+lib/SFML/Graphics/Vertex.hpp:
+
+lib/SFML/Graphics/Color.hpp:
+
+lib/SFML/Graphics/PrimitiveType.hpp:
+
+lib/SFML/Graphics/ConvexShape.hpp:
+
+lib/SFML/Graphics/Font.hpp:
+
+lib/SFML/Graphics/Glyph.hpp:
+
+lib/SFML/Graphics/Texture.hpp:
+
+lib/SFML/Graphics/Image.hpp:
+
+lib/SFML/Graphics/RectangleShape.hpp:
+
+lib/SFML/Graphics/RenderTarget.hpp:
+
+lib/SFML/Graphics/View.hpp:
+
+lib/SFML/Graphics/RenderTexture.hpp:
+
+lib/SFML/Graphics/RenderWindow.hpp:
+
+lib/SFML/Graphics/Shader.hpp:
+
+lib/SFML/Graphics/Glsl.hpp:
+
+lib/SFML/Graphics/Glsl.inl:
+
+lib/SFML/Graphics/Sprite.hpp:
+
+lib/SFML/Graphics/Text.hpp:
+
+lib/SFML/Graphics/VertexBuffer.hpp:
+
+lib/SFML/Audio.hpp:
+
+lib/SFML/Audio/InputSoundFile.hpp:
+
+lib/SFML/Audio/Export.hpp:
+
+lib/SFML/Audio/Listener.hpp:
+
+lib/SFML/Audio/Music.hpp:
+
+lib/SFML/Audio/SoundStream.hpp:
+
+lib/SFML/Audio/SoundSource.hpp:
+
+lib/SFML/Audio/AlResource.hpp:
+
+lib/SFML/Audio/OutputSoundFile.hpp:
+
+lib/SFML/Audio/Sound.hpp:
+
+lib/SFML/Audio/SoundBuffer.hpp:
+
+lib/SFML/Audio/SoundBufferRecorder.hpp:
+
+lib/SFML/Audio/SoundRecorder.hpp:
+
+lib/SFML/Audio/SoundFileFactory.hpp:
+
+lib/SFML/Audio/SoundFileFactory.inl:
+
+lib/SFML/Audio/SoundFileReader.hpp:
+
+lib/SFML/Audio/SoundFileWriter.hpp:
+
+src/obstacle.h:
+
+src/player.h:
+
+src/score.h:
+
+src/settings.h:
